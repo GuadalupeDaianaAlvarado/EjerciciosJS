358f5e5527c73b543c11fea4a3133cdf2691d1fe
GuadalupeDaianaAlvarado <dai.221294@gmail.com>
2023-04-28 00:38:57 -0300

    Ejercicios-1-10-JS

---
+++ assest/scrip/app.js
@@ -0,0 +1,45 @@
+let miNombre= "Guadalupe"
+console.log(miNombre)
+
+let miApellido= "Alvarado"
+console.log(miApellido)
+
+let miEdad=(28)
+console.log(miEdad)
+
+let miMascota= "Perrita Lola"
+console.log(miMascota)
+
+let edadMascota= "5"
+console.log(edadMascota)
+
+let nombreCompleto=(miNombre&miApellido)
+console.log(nombreCompleto)
+
+let suma=(miEdad+edadMascota)
+console.log(suma)
+
+let resta=(miEdad-edadMascota)
+console.log(resta)
+
+let multiplicacion=(miEdad*edadMascota)
+console.log(multiplicacion)
+
+let division=(miEdad/edadMascota)
+console.log(division)
+
+let textoPresentacion=(nombreCompleto+miEdad+miMascota+edadMascota)
+console.log(textoPresentacion)
+
+
+/* let persona ={ 
+    nombre: "Daiana",
+    apellido: "Alvarado",
+    edad: 28
+}
+console.log(persona)
+
+console.log(persona.nombre)
+let nombreIngresado = prompt("ingrese su nombre:")
+console.log(nombreIngresado)
+alert("Hola, gracias por visitarnos " + "" + nombreIngresado) */
